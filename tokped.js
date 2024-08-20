84eb5ce959d823f390b5e40b265cb8448536a5be
gaizkatvofficial <101855980+gaizkatvofficial@users.noreply.github.com>
2024-08-20 14:01:08 +0700

    Update tokped.js

--- tokped.js
+++ tokped.js
@@ -1,10 +1,1 @@
-// Ubah URL tujuan dengan URL afiliasi Tokopedia Anda
-let origin = 'https://tokopedia.link/hvStQEJ0cMb';
-
-window.pux = new window.dpu(target, {
-    newTab: true,
-    cookieExpires: 60 * 24 / 1,
-    afterOpen: function () {
-        window.location.href = origin; // Arahkan ke URL afiliasi setelah pop-under dibuka
-    }
-});
+let affiliateUrl = "https://tokopedia.link/hvStQEJ0cMb"; // URL afiliasi Tokopedia yang benar
